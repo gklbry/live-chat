7ba4f832498703b7ed9a2319a3aad690335f636c
Максим Хворостяный <gekelberry@MacBook-Pro-Maksim.local>
2023-12-23 05:05:23 +0500

    create server

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'static'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  const prismaService = app.get(PrismaService);
+  app.enableShutdownHooks();
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
